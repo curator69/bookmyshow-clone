70d9f08ccc5f22c666c0da99479241b4b65ddab2
curator69 <75662004+curator69@users.noreply.github.com>
2022-09-24 23:34:16 +0530

    updated webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,6 @@ module.exports = {
         exclude: "/node_modules/",
         use: "babel-loader",
       },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(["css-loader", "sass-loader"]),
-      },
     ],
   },
 };
