4b06ae4dd0d4e20dddf2f5948f62278f2266c3ca
curator69 <75662004+curator69@users.noreply.github.com>
2022-09-28 01:32:18 +0530

    markup complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 
+import logo from "../assets/images/bookmyshow-logo-vector.png";
+import searchIcon from "../assets/icons/search-outline.svg";
+import dropDownIcon from "../assets/icons/caret-down-outline.svg";
+import hamburgerIcon from "../assets/icons/reorder-three-outline.svg";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <div className="navigation-primary">
+        <div className="nav-primary--left">
+          <img className="main-logo" src={logo} alt="logo" />
+          <div>
+            <img src={searchIcon} alt="searchIcon" />
+            <input
+              type="text"
+              placeholder="Search for Movies, Plays, Sports and Activities"
+            />
+          </div>
+        </div>
+        <div className="nav-primary--right">
+          <div>
+            <p>Mumbai</p>
+            <img src={dropDownIcon} alt="dropDownIcon" />
+          </div>
+          <a href="#">Sign In</a>
+          <img src={hamburgerIcon} alt="hamburgerIcon" />
+        </div>
+      </div>
+      <div className="nav-secondary">
+        <div className="nav-secondary--left">
+          <ul>
+            <li>
+              <a href="#">Mumbai</a>
+              <a href="#">Stream</a>
+              <a href="#">Events</a>
+              <a href="#">Plays</a>
+              <a href="#">Sports</a>
+              <a href="#">Activites</a>
+              <a href="#">Buzz</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-secondary--right">
+          <ul>
+            <li>
+              <a href="#">List Your Show</a>
+              <a href="#">Corporates</a>
+              <a href="#">Offers</a>
+              <a href="#">Gift Cards</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
